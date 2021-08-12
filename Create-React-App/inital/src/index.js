b3e60e939698314e0030240496afe9d661297a15
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-12 23:06:04 +0530

    create first component

--- Create-React-App/inital/src/index.js
+++ Create-React-App/inital/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react" //this code not specific to react app
+import ReactDom from "react-dom" //need to use .render() method
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greeting() { //Greeting is a component and component name has to start with capital letter
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return <h4>This is Uttam , Hello there</h4> //each component should return something genrally JSX
+}
+
+ReactDom.render(<Greeting/> , document.getElementById("root"))
+
+//.render() takes two parameter , a.the component which needs to render. b.the place where it needs to be rendered
