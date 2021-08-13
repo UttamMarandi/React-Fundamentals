954eea471a9720fa8e35743b851c07e21304b6f9
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-13 22:34:27 +0530

    nested components , react developer tools , create booklist

--- Create-React-App/inital/src/index.js
+++ Create-React-App/inital/src/index.js
@@ -1,6 +1,8 @@
 import React from "react" //this code not specific to react app
 import ReactDom from "react-dom" //need to use .render() method
 
+//SESSION1
+
 // function Greeting() { //Greeting is a component and component name has to start with capital letter
 
 //   return (
@@ -27,35 +29,91 @@ import ReactDom from "react-dom" //need to use .render() method
 
 //JSX RULES
 //it always returns a single element , meaning return can't have two adjacent div . It can have only one
-//we can use React.Fragment (<> </>) to not add any additonal div
+//we can use React.Fragment (<> </>) instead of  adding any additonal div
 //<> </> are React.Fragment
-function Greeting() {
-
-  return (
-  <> 
-    <div>
-        <h2>Hello there, this is uttam</h2>
-        <ul>
-            <li>
-                <a href="asombrosodesign.com">Check my website</a>
-            </li>
-        </ul>
-    </div>
- </>
- 
- 
-//   <div>//not possible
-//       <h2>how</h2>
-//   </div>
-  )
-  //as long as one div within return it is okk.
- 
-}
+//use CAMELCASE for html attribute , ex-onClick , as we are working on a JS file ..so need some kind of diff
+//use className instead of class
 
 
-ReactDom.render(<Greeting/> , document.getElementById("root"))
+//NESTED COMPONENTS , REACT TOOLS
+
+// function Greeting() {
+
+//   return (
+//   <> 
+//     <div>
+//         <Person/>
+//         <Message/>
+//     </div>
+//  </>
+ 
+ 
+// //   <div>//not possible
+// //       <h2>how</h2>
+// //   </div>
+//   )
+//   //as long as one div within return it is okk.
+ 
+// }
+
+// const Person = () => <h2>John Doe</h2>
+// const Message = () => {
+//     return(
+//         <p>this is my passage</p>
+//     )
+// }
+
+
+// ReactDom.render(<Greeting/> , document.getElementById("root"))
+
 
 //Greeting is a stateless functional component meaning it has no state
 //we always return JSX
 
 //.render() takes two parameter , a.the component which needs to render. b.the place where it needs to be rendered
+
+
+///SESSION 2
+//CREATE A BOOKLIST
+
+function BookList() {
+    return(
+        <section>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    )
+}
+
+const Book = () => {
+    return(
+        <article>
+            <Img/>
+            <Title/>
+            <Author/>
+        </article>
+    )
+}
+const Img = () => {
+    return(
+        <img src="https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg" alt="img" />
+    ) 
+}
+
+const Title = () => {
+    return(
+        <h2>Word Power Made Easy Paperback</h2>
+    )
+}
+
+const Author = () => {
+    return(
+        <h3>Norman Lewis</h3>
+    )
+}
+
+
+ReactDom.render(<BookList/> , document.getElementById("root"))
