11a4af34701c61768ac9026e0a31e5963a5ec08e
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-14 01:01:46 +0530

    pass parameter to components , use object for book data , destructuing

--- Create-React-App/inital/src/index.js
+++ Create-React-App/inital/src/index.js
@@ -81,6 +81,26 @@ import "./index.css"
 ///SESSION 2
 //CREATE A BOOKLIST
 
+
+
+//vars
+const title = "Word Power Made Easy : using variable"
+const imgur = "https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg"
+const author = "Norman Lewis : using variable"
+
+//use object for second book
+const secondBook = {
+    title : "Billy Summers",
+    author : "Stephen King",
+    imgur : "https://images-na.ssl-images-amazon.com/images/I/51vSbWpF+dS._SY344_BO1,204,203,200_.jpg",
+}
+
+const thirdBook = {
+    title : "Atomic Habits",
+    author : "James Clear",
+    imgur : "https://images-na.ssl-images-amazon.com/images/I/51EU9naCcwL._SX325_BO1,204,203,200_.jpg",
+}
+
 function BookList() {
     return(
         <section className= "booklist"> 
@@ -92,11 +112,16 @@ function BookList() {
             <Book2/>
             <Book2/>
             <Book2/>
+        
+            <Book3 title = {secondBook.title} imgur = {secondBook.imgur} author = {secondBook.author} />
+            <Book3 title = {thirdBook.title} imgur = {thirdBook.imgur} author = {thirdBook.author}/>
+            <Book3/>
+
 
         </section>
     )
 }
-
+//age = {22} b.c we are entering into js world to access number
 const Book = () => {
     return(
         <article className="book">
@@ -108,7 +133,7 @@ const Book = () => {
 }
 const Img = () => {
     return(
-        <img src="https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg" alt="img" />
+        <img src="https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg" alt = " " />
     ) 
 }
 
@@ -129,20 +154,41 @@ const Author = () => {
 
 
 //Create book type2 and remove nested components and add img, title, and name in one compnoent
-const author = "Norman Lewis: using variable"
+
 const Book2 =() => {
-    const title = "Word Power Made Easy : using variable"
+    
     return(
         <article className="book">
-            <img src="https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg" alt="img" />
+            <img src={imgur} alt="img" />
             <h2>{title}</h2> 
-            <h3>{author}</h3>
+            <h3>{author.toUpperCase()}</h3>
 
         </article>
     )
 }
 //acess variable within jsx using {} braces
-//but we can't use expression within {} b.c it is within return
+//but we can't use statements like assigments within {} b.c it is within return
+
+
+//Create Book3 and add parameter to make book more dynamic
+
+const Book3 =({imgur,author,title}) => { //props value is set up in the place where the component is used
+    
+    //destructuring props
+    // const {imgur , author , title} = props
+    //we can do the above directky in parameter
+
+    return(
+        <article className="book">
+            <img src={imgur} alt="img" />
+            <h2>{title}</h2> 
+            <h3>{author}</h3>
+            
+        </article>
+    )
+}
+//for the values that we have passed , we can access them using props.key
+//only the component that passed the key=value can access it 
 
 
 ReactDom.render(<BookList/> , document.getElementById("root"))
