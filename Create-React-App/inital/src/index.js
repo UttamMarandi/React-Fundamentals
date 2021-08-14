abb0f7fe5f55a9b6a3b382cd494011ddfe997c41
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-14 14:31:21 +0530

    set up event, reference & inline event , passing argument to event method

--- Create-React-App/inital/src/index.js
+++ Create-React-App/inital/src/index.js
@@ -141,7 +141,16 @@ function BookList() {
 
                 )
             })}
-            
+            {/* set up event */}
+            {books.map((book)=>{
+                return(
+                    <Book6 key ={book.id} {...book}> 
+
+                    </Book6>
+
+                )
+            })}
+
             
             
         </section>
@@ -279,6 +288,37 @@ const Book5 = (props) => { //here props is not the object but all the properties
     )
 } 
 
+
+//Iteration 6
+//Set up an event
+const Book6 = (props) => { 
+    const {author,title,imgur} = props
+    const clickHandler = (e) => { //reference example
+        alert("book selected")
+        console.log(e); //e is event object
+        console.log(e.target); ///e.target shows the html element that is clicked
+        
+    }
+    const complexExample = (e,author) => {
+        console.log(author); //author gets logged the moment DOM is rendered. this is not expected for a click event
+        //If we have to pass an argument to our reference function then we must wrap it inside an arrow function in onClick statement
+        
+    }
+
+    return(
+        <article className="book">
+            <img src={imgur} alt="img" />
+            <h2 onClick={()=> console.log(title)}>{title}</h2> 
+            <h3>{author}</h3>
+            <button type="button" onClick={clickHandler}>Clcik Me : Reference example</button>
+            <button onClick={()=> alert("book selected inline")}>Click Me Inline example</button>
+            {/* Inline example */}
+            <h2></h2>
+            <button type="button" onClick={()=>complexExample(author)}>More Complex example</button>
+            {/* //If we have to pass an argument here (author) to our reference function then we must wrap it inside an arrow function in onClick statement */}
+        </article>
+    )
+} 
 
 
 
