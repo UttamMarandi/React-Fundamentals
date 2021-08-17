438c48b05fdaec2c189ff8b27cbeac0bb0d002d4
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-17 19:34:01 +0530

    access input value , value attribute, onChange

--- React Advanced/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React Advanced/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,16 @@ import React, { useState } from 'react';
 // React
 // value, onChange
 
+//In order to access the input value we use useState to create value and onChange
+//on input tag set the value to attribute to the useState value 
+//now the value on form depends on the useState value
+//set up onChange event on the input value.
+//onChange event takes a callback function within which we call the setFunciton and set it to e.target.value 
+
 const ControlledInputs = () => {
+  
+  const [firstName , setFirstName] = useState("")
+  const [email,setEmail] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault() //we need to prevent thr browser's default funcitonality of submitting
@@ -18,7 +27,7 @@ const ControlledInputs = () => {
     e.preventDefault() 
     console.log("Hello World from handleCLick");
   }
-
+  console.log("firstName value",firstName);
   return (
     // In react we have two options , either we can add onSubit to form to submit the button, or we can add an onClick to the button within form tag
     //if we use onClick on form , then Hello World is logged the moment we click on any area of the form
@@ -28,11 +37,11 @@ const ControlledInputs = () => {
       <form className="form" onSubmit = {handleSubmit}>
       <div className="form-control">
         <label htmlFor="firstName">Name : </label>
-        <input type="text" id="firstName" name = "firstName" />
+        <input type="text" id="firstName" name = "firstName" value = {firstName} onChange={(e)=>{setFirstName(e.target.value)}} />
       </div>
       <div className="form-control">
         <label htmlFor="email">Email : </label>
-        <input type="text" id="email" name = "email" />
+        <input type="text" id="email" name = "email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
       </div>
       <button className="btn" onClick= {handleClick}>Add Person</button>
       </form>
