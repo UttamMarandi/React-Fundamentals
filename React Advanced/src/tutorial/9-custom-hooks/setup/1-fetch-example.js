e28c3a6ae2cad1fa6a07ba2b715ea8e2628ae942
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-19 12:09:53 +0530

    understanding custom hooks

--- React Advanced/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ React Advanced/src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './2-useFetch'
 
+//custom hooks allow us to reuse the functionality 
+
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
 
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
+  const {loading , products} = useFetch(url)
+  // useFetch is our custom hooks ,
+  //the functiolity of useFetch is not dependent on the component , we can just use fetch by paasing the url
+  //we need to name the fetch hook with prefix use , as in react a function can be a component and custom hooks
 
-  useEffect(() => {
-    getProducts()
-  }, [url])
   console.log(products)
   return (
     <div>
--- React Advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ React Advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,24 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    const response = await fetch(url)
+    const products = await response.json()
+    setProducts(products)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [url])
+  //get products method is called on initial load or when we change the url ,
+
+  //in order to convert a functionaliity to a custom hook , first we need to copy the component code except the return statment into our custom functin=on.
+  //then it needs to return the useState values i.e loading and url
+ 
+  return ({loading , products})
+};
